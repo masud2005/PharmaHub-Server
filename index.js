a689600a222ccc21e8f7a7f51de332884189c58a
masud2005 <masud20012005@gmail.com>
2025-01-19 00:23:20 +0600

    Users Role API Create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -41,7 +41,15 @@ async function run() {
             res.send(result);
         })
 
-        
+        app.patch('/users/:id/role', async (req, res) => {
+            const { id } = req.params;
+            const { role } = req.body;
+            const result = await userCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: { role: role } }
+            );
+            res.send(result);
+        });
 
 
         // Send a ping to confirm a successful connection
